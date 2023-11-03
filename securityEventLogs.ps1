32b6a72dba6f91e340e3d53f5e69a4fd62775374
Lorenzo Zimmerer <zozohatch@gmail.com>
2023-11-03 14:19:06 -0400

    adding week 8 prject files

---
+++ securityEventLogs.ps1
@@ -0,0 +1,14 @@
+﻿# Storyline: Review the Security Event Log
+
+# List all the Available Windows Event Logs
+Get-EventLog -List 
+
+# prompt the user to selevt the log to view
+$readLog = Read-Host -Prompt "Please select a log to review from the list above"
+
+# prompt the user for a string to search for in the log
+$searchPhrase = Read-Host -Prompt "Please enter a phrase to filter the logs by"
+
+# Print the results of the log
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike $searchPhrase } | Export-Csv -NoTypeInformation `
+-Path "C:\Users\champuser\Desktop\securityLogs.csv"
---
+++ sendEmail.ps1
@@ -0,0 +1,20 @@
+﻿
+# Storyline: send an email
+
+# Body of the email
+$msg = "Hello there <3"
+
+# echo to the screen
+Write-Host -BackgroundColor Cyan -ForegroundColor Magenta $msg
+
+# Subject
+$sbj = "Greetings"
+
+# From Address
+$fromEmail = "lorenzo.zimmerer@mymail.champlain.edu"
+
+# To Address
+$toEmail = "deployer@csi-web"
+
+# Sending the Email
+Send-MailMessage -From $fromEmail -To $toEmail -Subject $sbj -Body $msg -SmtpServer 192.168.6.21
