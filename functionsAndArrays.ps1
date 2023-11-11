3bcf9fe93923b08b2a415a37999421d0357b4435
Lorenzo Zimmerer <zozohatch@gmail.com>
2023-11-11 13:43:22 -0500

    adding week 10 files

---
+++ functionsAndArrays.ps1
@@ -0,0 +1,96 @@
+﻿# Storyline: view the event logs, check for valid log, and print the results
+
+
+function select_log() {
+
+    cls
+
+    # List all event logs
+    $theLogs = Get-EventLog -List | select Log
+    $theLogs | Out-Host
+
+    # Initialize the array to store the logs 
+    $arrLog = @()
+
+    foreach ($tempLog in $theLogs) {
+
+        # Add each log to the array
+        # NOTE: These are stored in the array as a hashtable in the format:
+        # @{Log=LOGNAME}
+        $arrLog += $tempLog
+        
+    } # end foreach
+
+
+    #Test that the array is populated 
+    #$arrlog
+
+    # Prompt the user for the log to view or quit.
+    $readLog = Read-Host -Prompt "Please enter a log from the list above or 'q' to quit the program"
+
+
+
+    # Check if the user wants to quit.
+    if ($readLog -match "^[qQ]$") {
+
+        # Stop executing the program and close the script
+        break
+
+    } # end if
+
+
+    log_check -logToSearch $readLog
+
+} # end function select_log()
+
+
+function log_check() {
+    
+    # String the user types in within the select_log function
+    Param([string]$logToSearch)
+
+    # Format the user input
+    # Example: @{Log=Security}
+    $theLog = "^{Log=" + $logToSearch + "}$"
+
+    # Search the array for the exact hashtable string
+    if ($arrLog -match $theLog) {
+
+        Write-Host -BackgroundColor Green -ForegroundColor White "Please wait, it may take a few moments to reteve the log entries."
+        sleep 2
+
+        # Call the function to view the log
+        view_log -logToSearch $logToSearch
+
+    } else {
+    
+        Write-Host -BackgroundColor Red -ForegroundColor White "The log specified doesn't exist."
+        sleep 2
+
+        select_log
+
+    } # end if
+
+
+} # end function log_check()
+
+
+
+function view_log() {    
+
+    cls
+
+    # Get the logs
+    Get-EventLog -Log $logToSearch -Newest 10 -After "1/1/2023"
+
+    # Pause the screen and wait until the user is ready to proceed
+    Read-Host -Prompt "Press enter when you are done"
+
+    # Go back to select_log
+    select_log
+
+} # end function view_log()
+
+
+# Run select_log as the first function
+select_log
---
+++ getServiceLogs.ps1
@@ -0,0 +1,75 @@
+﻿# Storyline: List all services in the system and show all, stopped, or running
+
+function select_service() {
+
+    cls      
+
+    $arrValidInput = @('all', 'stopped', 'running')
+
+    $readInput = Read-Host -Prompt "Please spcify if you want to see 'all', 'running', or 'stopped' services, or 'quit' to quit"
+
+    
+    # Check if the user wants to quit.
+    if ($readInput -imatch "quit") {
+
+        # Stop executing the program and close the script
+        break
+
+    } # end if
+
+    # Check if the user entered a valid option
+    if (-not $readInput -iin $arrValidInput) {
+
+        Write-Host -BackgroundColor Red -ForegroundColor White "Please enter a vaid option."
+        sleep 2
+
+        # Return to option input after a short delay
+        select_service
+
+    } # end if
+
+    # send input to show_service
+    show_service -userSelection $readInput
+
+
+} # end function select_services
+
+
+function show_service() {
+    
+    Param([string]$userSelection)
+
+    if($userSelection -imatch $arrValidInput[0]) {
+        
+        Write-Host -BackgroundColor Green -ForegroundColor White "Showing " $arrValidInput[0] " services"
+        sleep 2
+
+        Get-Service
+
+    } elseif($userSelection -imatch $arrValidInput[1]) { 
+
+        Write-Host -BackgroundColor Green -ForegroundColor White "Showing " $arrValidInput[1] " services"
+        sleep 2
+
+        Get-Service | Where { $_.Status -eq "Stopped" }
+
+    } elseif($userSelection -imatch $arrValidInput[2]) {
+
+        Write-Host -BackgroundColor Green -ForegroundColor White "Showing "  $arrValidInput[2] " services"
+        sleep 2
+
+        Get-Service | Where { $_.Status -eq "Running" }
+
+    } # end if
+
+     # Pause the screen and wait until the user is ready to proceed
+    Read-Host -Prompt "Press enter when you are done"
+
+    # Go back to select_log
+    select_service
+
+} # end function show_service
+
+select_service
+
+
